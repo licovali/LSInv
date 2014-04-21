5c87d74a445d942a0cd145247b581742908f39e3
licovali <vlicovali@gmail.com>
2014-04-21 14:44:21 -0500

    Users
    Model and Store

---
+++ app/model/security/User.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="~/ext/ext-all-debug.js" />
+Ext.define('LSInv.model.security.User', {
+    extend: 'Ext.data.Model',
+    idProperty: 'id',
+    fields: [
+        { name: 'id' },
+        { name: 'user' },
+        { name: 'username' },
+        { name: 'role_id'}
+    ]
+});
---
+++ app/store/security/Users.js
@@ -0,0 +1,8 @@
+﻿Ext.define('LSInv.store.security.Users', {
+    extend: 'Ext.data.Store',
+    requires: [
+        'LSInv.model.security.User'
+    ],
+    model: 'LSInv.model.security.User',
+    autoLoad: false
+});
